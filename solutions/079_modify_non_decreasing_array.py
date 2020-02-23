aecf473b0f05f33bd5570d2098b69a1b1be8c18c
Shuguang Yan <yanshg@yahoo.com>
2020-02-22 17:51:05 -0800

    update 079

--- solutions/079_modify_non_decreasing_array.py
+++ solutions/079_modify_non_decreasing_array.py
@@ -12,15 +12,36 @@ Given the array [10, 5, 1], you should return false, since we can't modify any o
 
 """
 
-def modify_non_decreasing_arr(nums,modified=False):
-    if not nums or len(nums)==1:
-        return True
+# O(N), space O(1)
 
-    if nums[0]<=nums[1]:
-        return modify_non_decreasing_arr(nums[1:],modified)
+# If no index that A[i]>A[i+1], return True
+#
+# If two or more indices that A[i]>A[i+1], return False
+#
+# If only one index that A[i]>A[i+1], then need check surrounding elements:
+#
+#   *  if i==0 or i==len(nums)-2, then return True, since we can modify A[i]=A[i+1]
+#   *  Then the case become A[i-1]<A[i]>A[i+1]<A[i+2], if A[i-1]>A[i+1] or A[i]>A[i+2], return False
+#   *  other cases return True since we can modify A[i]=A[i+1]
 
-    return False if modified else modify_non_decreasing_arr(nums[1:],True)
+
+def modify_non_decreasing_arr(nums):
+    n=len(nums)
+
+    p=None
+    for i in range(len(nums)-1):
+        if nums[i]>nums[i+1]:
+            if p is not None:
+                return False
+            p=i
+
+    return p is None or p==0 or p==n-2 or \
+            nums[p-1]<=nums[p+1] or nums[p]<=nums[p+2]
 
 assert modify_non_decreasing_arr([10, 5, 7])
 assert not modify_non_decreasing_arr([10, 5, 1])
+assert not modify_non_decreasing_arr([2, 5, 1])
+assert not modify_non_decreasing_arr([4, 5, 3, 4, 6])
+
+
 
