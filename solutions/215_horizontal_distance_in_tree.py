23c4386281fb73a239606763fef88f6f5858ed2c
Shuguang Yan <yanshg@yahoo.com>
2020-03-10 13:24:15 -0700

    Add 215

--- solutions/215_horizontal_distance_in_tree.py
+++ solutions/215_horizontal_distance_in_tree.py
@@ -40,10 +40,18 @@ class Node:
     def __repr__(self):
         return "{}->({},{})".format(self.val,self.left,self.right)
 
-def get_horizontal_distance(root,distances=dict()):
-    
-def bottom_view_of_tree(root):
-    pass
+def helper(root,hd=0,hds=dict()):
+    if not root:
+        return
+
+    helper(root.left,hd-1,hds)
+    hds[hd]=root.val
+    helper(root.right,hd+1,hds)
+
+def get_bottom_view(root):
+    hds=dict()
+    helper(root,0,hds)
+    return [ x[1] for x in sorted(hds.items(), key=lambda x:x[0]) ]
 
 n1=Node(5)
 n2=Node(3)
@@ -55,6 +63,13 @@ n7=Node(9)
 n8=Node(0)
 n9=Node(8)
 
-
 n1.left=n2
 n1.right=n3
+n2.left=n4
+n2.right=n5
+n3.left=n6
+n3.right=n7
+n4.left=n8
+n7.left=n9
+
+assert get_bottom_view(n1)==[0,1,3,6,8,9]
