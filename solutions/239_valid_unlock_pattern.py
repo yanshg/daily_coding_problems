f043288b8588e6564537810987a15d54ec214b32
Shuguang Yan <yanshg@yahoo.com>
2020-05-09 00:10:28 -0700

    Update 239

--- solutions/239_valid_unlock_pattern.py
+++ solutions/239_valid_unlock_pattern.py
@@ -17,19 +17,21 @@ Find the total number of valid unlock patterns of length N, where 1 <= N <= 9.
 
 """
 
-# DFS
+# Idea:  DFS
+#
+# source_key: [ (target_key, jump_key) ]
+# jump_key is '', means the target_key is directly connected from source_key
 
 matrix={
-    # source_key: [ (target_key, jumped_key)]
-    '1': [('2','0'),('4','0'),('5','0'),('3','2'),('7','4'),('9','5')],
-    '3': [('2','0'),('5','0'),('6','0'),('1','2'),('7','5'),('9','6')],
-    '7': [('4','0'),('5','0'),('8','0'),('1','4'),('3','5'),('9','8')],
-    '9': [('6','0'),('5','0'),('8','0'),('3','6'),('7','8'),('1','5')],
-    '2': [('1','0'),('3','0'),('4','0'),('5','0'),('6','0'),('8','5')],
-    '4': [('1','0'),('2','0'),('5','0'),('7','0'),('8','0'),('6','5')],
-    '6': [('2','0'),('3','0'),('5','0'),('8','0'),('9','0'),('4','5')],
-    '8': [('4','0'),('5','0'),('6','0'),('7','0'),('9','0'),('2','5')],
-    '5': [('1','0'),('2','0'),('3','0'),('4','0'),('6','0'),('7','0'),('8','0'),('9','0')],
+    '1': [('2',''),('4',''),('5',''),('3','2'),('7','4'),('9','5')],
+    '3': [('2',''),('5',''),('6',''),('1','2'),('7','5'),('9','6')],
+    '7': [('4',''),('5',''),('8',''),('1','4'),('3','5'),('9','8')],
+    '9': [('6',''),('5',''),('8',''),('3','6'),('7','8'),('1','5')],
+    '2': [('1',''),('3',''),('4',''),('5',''),('6',''),('8','5')],
+    '4': [('1',''),('2',''),('5',''),('7',''),('8',''),('6','5')],
+    '6': [('2',''),('3',''),('5',''),('8',''),('9',''),('4','5')],
+    '8': [('4',''),('5',''),('6',''),('7',''),('9',''),('2','5')],
+    '5': [('1',''),('2',''),('3',''),('4',''),('6',''),('7',''),('8',''),('9','')],
 }
 
 # DFS
@@ -45,16 +47,19 @@ def get_valid_pattern_nums(matrix,n,start,length,path,visited):
     ways=0
     for target,jump in matrix[start]:
         if target not in visited and \
-            (jump=='0' or jump in visited):
+            (not jump or jump in visited):
             ways+=get_valid_pattern_nums(matrix,n,target,length+1,path+[target],visited.copy())
     return ways
 
 def get_total_valid_pattern_nums(matrix,n):
+    # 1, 3, 7, 9 are symmetric so multiplying by 4
+    # 2, 4, 6, 8 are symmetric so multiplying by 4
     return 4 * get_valid_pattern_nums(matrix,n,'1',1,['1'],set()) + \
            4 * get_valid_pattern_nums(matrix,n,'2',1,['2'],set()) + \
            get_valid_pattern_nums(matrix,n,'5',1,['5'],set())
 
 assert get_total_valid_pattern_nums(matrix,1)==9
+assert get_total_valid_pattern_nums(matrix,2)==40
 assert get_total_valid_pattern_nums(matrix,3)==176
 assert get_total_valid_pattern_nums(matrix,4)==648
 assert get_total_valid_pattern_nums(matrix,5)==2040
