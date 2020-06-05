8db2a812fc0563a536e8eb746f00b76663d08210
Shuguang Yan <yanshg@yahoo.com>
2020-06-05 10:09:25 -0700

    Add 366 367 373 374

---
+++ solutions/373_consecutive_numbers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""
+
+This problem was asked by Facebook.
+
+Given a list of integers L, find the maximum length of a sequence of consecutive numbers that can be formed using elements from L.
+
+For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we can build a sequence [1, 2, 3, 4, 5] which has length 5.
+
+"""
+
+# Same problem with 099_longest_consecutive_items.py
