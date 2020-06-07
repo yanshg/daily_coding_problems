9dd28b154364447be4f301026f883b752e2d70e5
Shuguang Yan <yanshg@yahoo.com>
2020-06-06 22:10:54 -0700

    Add 378

---
+++ solutions/378_json_encode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+
+This problem was asked by Coinbase.
+
+Write a function that takes in a number, string, list, or dictionary and returns its JSON encoding. It should also handle nulls.
+
+For example, given the following input:
+
+[None, 123, ["a", "b"], {"c":"d"}]
+
+You should return the following, as a string:
+
+'[null, 123, ["a", "b"], {"c": "d"}]'
+
+"""
+
+import json
+
+assert json.dumps([None, 123, ["a", "b"], {"c":"d"}]) == '[null, 123, ["a", "b"], {"c": "d"}]'
+
