b1ebffdc4324faf083d28d0566f63e9b864c529e
Shuguang Yan <yanshg@yahoo.com>
2020-06-14 09:07:47 -0700

    update 209

--- solutions/209_longest_common_subsequence.py
+++ solutions/209_longest_common_subsequence.py
@@ -11,12 +11,12 @@ Write a program that computes the length of the longest common subsequence of th
 def helper(strings,sequence,indices):
     #print("sequence:",sequence)
 
-    lcs=len(sequence)
+    lcs_len=len(sequence)
     chars=set((strings[0])[indices[0]+1:])
 
     for c in chars:
         # if c is in all the strings' substring ( (strings[i])[indices[i]+1:] )
-        #     lcs=min(lcs, helper(strings,sequence+c,new_indices))
+        #     lcs_len=max(lcs_len, helper(strings,sequence+c,new_indices))
         # else:
         #     continue with next char
         new_indices=list()
@@ -27,11 +27,11 @@ def helper(strings,sequence,indices):
             new_indices+=[index]
 
         if len(new_indices)==len(strings):
-            lcs=max(lcs,helper(strings,sequence+c,new_indices))
+            lcs_len=max(lcs_len,helper(strings,sequence+c,new_indices))
 
-    return lcs
+    return lcs_len
 
-def get_lcs(strings):
+def get_lcs_len(strings):
     return helper(strings,'',[-1]*len(strings))
 
-assert get_lcs(["epidemiologist", "refrigeration", "supercalifragilisticexpialodocious"])==5
+assert get_lcs_len(["epidemiologist", "refrigeration", "supercalifragilisticexpialodocious"])==5
