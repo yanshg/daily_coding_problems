c69794f02d1370d5ef2642add4ad9c6853798d85
yanshg <yanshg@yahoo.com>
2020-08-31 00:28:44 -0700

    Update 052

--- solutions/052_LRU_cache.py
+++ solutions/052_LRU_cache.py
@@ -4,7 +4,7 @@
 
 This problem was asked by Google.
 
-Implement an LRU (Least Recently Used) cache. It should be able to be initialized with a cache size n, and contain the following methods:
+Implement an LRUCache (Least Recently Used) cache. It should be able to be initialized with a cache size n, and contain the following methods:
 
     set(key, value): sets key to value. If there are already n items in the cache and we are adding a new item, then it should also remove the least recently used item.
     get(key): gets the value at key. If no such key exists, return null.
@@ -29,127 +29,91 @@ Each operation should run in O(1) time.
 # For Hashmap:  key1 -> Node1
 #               key2 -> Node2
 
-class Node:
-    def __init__(self,key,value,next=None,prev=None):
-        self.key=key
-        self.value=value
-        self.next=next
-        self.prev=prev
 
-    def __repr__(self):
-        return "({}=>{})<->{}".format(self.key,self.value,self.next)
+CAPACITY = 128
 
-# Double end queue specific for cache
-class Deque:
+class CacheNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class DoubleLinkedList:
     def __init__(self):
-        self.head=None
-        self.tail=None
-        self.size=0
+        self.head = CacheNode('HEAD', None)
+        self.tail = CacheNode('TAIL', None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def __repr__(self):
-        return str(self.head)
+        keys = list()
+        node = self.head.next
+        while node != self.tail:
+            keys += [ str(node.key) ]
+            node = node.next
+        return ','.join(keys)
 
-    def __len__(self):
-        return self.size
+    def add(self, node):
+        if node:
+            node.prev = self.head
+            node.next = self.head.next
+            node.next.prev = node
+            self.head.next = node
 
-    def append(self,node):
-        if not self.head:
-            self.head=node
-            node.prev=None
+    def remove(self, node):
+        if node:
+            node.prev.next = node.next
+            node.next.prev = node.prev
 
-        if self.tail:
-            self.tail.next=node
-            node.prev=self.tail
-        self.tail=node
+    def get_tail(self):
+        tail = self.tail.prev
+        return tail if tail != self.head else None
 
-        node.next=None
-        self.size+=1
+class LRUCache:
+    def __init__(self, capacity=CAPACITY):
+        self.capacity = capacity
+        self.hashmap = dict()
+        self.linkedlist = DoubleLinkedList()
 
-    def popleft(self):
-        return self.remove(self.head)
+    def __repr__(self):
+        return str(self.linkedlist)
 
-    def remove(self,node):
-        if not node:
+    def get(self, key):
+        if key not in self.hashmap:
             return None
 
-        prev_node=node.prev
-        next_node=node.next
-        if prev_node:
-            prev_node.next=next_node
-        else:
-            # removing the head node
-            self.head=next_node
+        node = self.hashmap[key]
+        linkedlist = self.linkedlist
+        linkedlist.remove(node)
+        linkedlist.add(node)
+        return node.value
 
-        if next_node:
-            next_node.prev=prev_node
-        else:
-            # removing the tail node
-            self.tail=prev_node
-
-        self.size-=1
-
-        # clean the node
-        node.prev=None
-        node.next=None
-
-        return node
-
-class LRUcache:
-    def __init__(self,n):
-        self.size=n
-        self.deque=Deque()
-        self.hashmap=dict()
-
-    def __repr__(self):
-        return str(self.deque)
-
-    def set(self,key,value):
-        # sets key to value.
-        # If there are already n items in the cache and we are adding a new item,
-        # then it should also remove the least recently used item.
-        if not key:
-            return
+    def put(self, key, value):
+        linkedlist = self.linkedlist
 
         if key in self.hashmap:
-            node=self.hashmap[key]
-            if node:
-                self.deque.remove(node)
-                self.deque.append(node)
-                node.value=value
-        else:
-            node=Node(key,value)
-            self.hashmap[key]=node
-            self.deque.append(node)
-            if len(self.deque)>self.size:
-                node_to_remove=self.deque.popleft()
-                if node_to_remove:
-                    del self.hashmap[node_to_remove.key]
+            node = self.hashmap[key]
+            linkedlist.remove(node)
+            linkedlist.add(node)
+            node.value = value
+            return
 
-    def get(self,key):
-        # gets the value at key. If no such key exists, return null.
-        if key and key in self.hashmap:
-            node=self.hashmap[key]
-            if node:
-                self.deque.remove(node)
-                self.deque.append(node)
-                return node.value
+        node = CacheNode(key, value)
+        linkedlist.add(node)
+        self.hashmap[key] = node
+        if len(self.hashmap) > self.capacity:
+            tail = linkedlist.get_tail()
+            if tail:
+                linkedlist.remove(tail)
+                del self.hashmap[tail.key]
 
-        return None
+cache = LRUCache(3)
+cache.put(1, 'hello1')
+cache.put(2, 'hello2')
+cache.put(3, 'hello3')
+cache.put(4, 'hello4')
 
-cache=LRUcache(3)
-cache.set('hello', 20)
-cache.set('world', 30)
-print("cache:", cache)
-
-assert cache.get('hello') == 20
-assert cache.get('world') == 30
-
-cache.set('haha', 'fine')
-cache.set('emmm', 'not good')
-
-print("cache:", cache)
-
-assert not cache.get('hello')
-assert cache.get('haha') == 'fine'
-assert cache.get('emmm') == 'not good'
+assert not cache.get(1)
+assert cache.get(4) == 'hello4'
 
