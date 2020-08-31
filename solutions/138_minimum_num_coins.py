60f4ba22e24a6b0e9f7267ce89abca2cc2a29e2d
yanshg <yanshg@yahoo.com>
2020-08-31 12:51:41 -0700

    update 138

--- solutions/138_minimum_num_coins.py
+++ solutions/138_minimum_num_coins.py
@@ -10,25 +10,48 @@ You can use standard American denominations, that is, 1c, 5c, 10c, and 25c.
 For example, given n = 16, return 3 since we can make it with a 10c, a 5c, and a 1c.
 """
 
-def get_min_coins(n):
-    coins=0
-    bases=[25,10,5,1]
-    for base in bases:
-        num=n//base
-        n-=num*base
-        coins+=num
-    return coins
+# Recursion problem issue:
+# 1. Find out the transforming pattern,
+# 2. Then use memo or DP table.
+#
+# Transform pattern:  f(n) = min(1+f(n-1), 1+f(n-5), 1+f(n-10), 1+f(n-25))
+#                or:  f(n) = 1 + min(f(n-1), f(n-5), f(n-10), f(n-25))
+#
+#        Base cases:  f(-n) = float('inf')
+#                     f(0) = 0
+
+
+# Idea 1: use memo from top to bottom
+
+def get_min_coins(n, memo=dict()):
+    if n<0:
+        return float('inf')
+    elif n==0:
+        return 0
+
+    if n in memo:
+        return memo[n]
+
+    memo[n] = 1 + min([ get_min_coins(n-cents) for cents in [1,5,10,25] ])
+    return memo[n]
 
 assert get_min_coins(16)==3
 
-# Using Dynamic programming methods
+# Idea 2: use DP table from bottom to top
 
-def get_min_coins_dp(n,bases=[]):
-    if not bases:
-        return 0
-    base=bases[0]
-    coins=n//base
-    remain=n-base*coins
-    return coins+get_min_coins_dp(remain,bases[1:])
+#             DP[i]:  means the minimum coins required to take i cents.
+#
+# Transform pattern:  DP[i] = 1 + min(DP[i-1], DP[i-5], DP[i-10], DP[i-25])
+#
+#        Base cases:  DP[0] = 0
+#
+#            Return:  DP[n]
 
-assert get_min_coins_dp(16,[25,10,5,1])==3
+def get_min_coins_dp(n):
+    DP = [0] * (n+1)
+    for i in range(1,n+1):
+        DP[i] = 1 + min([ DP[i-cents] for cents in [1,5,10,25] if i>=cents ])
+    return DP[n]
+
+assert get_min_coins(16)==3
+assert get_min_coins_dp(16)==3
