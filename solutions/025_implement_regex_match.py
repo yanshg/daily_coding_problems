9570f8e32224087ee344632e96e3c79793195c48
yanshg <yanshg@yahoo.com>
2020-09-26 21:40:50 -0700

    update 025

--- solutions/025_implement_regex_match.py
+++ solutions/025_implement_regex_match.py
@@ -17,12 +17,14 @@ Given the regular expression ".*at" and the string "chat", your function should 
 
 """
 
-# Idea: Use danymic programming,
+# Idea: Use recursion
+#
 #       Base cases: 1. zero length of regex
 #                   2. zero length of string
 #                   3. if first char in regex is '*', recursionly match * with zero or first 1 character in string.
 #                   4. first char is match, check remaining chars
 
+# O(2^n)
 def is_match(regex,string):
     if not regex:
         return not string
@@ -40,6 +42,43 @@ def is_match(regex,string):
 
     return False
 
+
+# Idea: Use DP table
+
+# DP[i][j]: True if match regex[:i+1] and string[:j+1] else False
+#
+# DP[i][j] = |- DP[i-1][j] or DP[i][j-1]  if regex[i] == '*'
+#            |- DP[i-1[j-1]               if regex[i] in [ '.', string[j] ]
+#
+# r' = '' + r
+# s' = '' + s
+# match(r', s') = match(r, s)
+#
+#            |- True                      if i==0 and j==0
+#            |- False                     if (i==0 and j>0) or (i>0 and j==0)
+# DP[i][j] = |- DP[i-1][j] or DP[i][j-1]  if regex[i-1] == '*'
+#            |- DP[i-1[j-1]               if regex[i-1] in [ '.', string[j-1] ]
+
+# O(m*n)
+def is_match_dp(regex, string):
+    m = len(regex)
+    n = len(string)
+    DP = [ [ False for j in range(n+1) ] for i in range(m+1) ]
+
+    # Handle base case that string is null
+    for i in range(1,m+1):
+        if regex[i-1] == '*':
+            DP[i][0] = DP[i-1][0]
+
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if regex[i-1] == '*':
+                DP[i][j] = DP[i-1][j] or DP[i][j-1]
+            elif regex[i-1] in [ '.', string[j-1] ]:
+                DP[i][j] = DP[i-1][j-1]
+
+    return DP[-1][-1]
+
 assert is_match(r'ra.', 'ray')
 assert not is_match(r'ra.', 'raymond')
 assert is_match(r'*at', 'chat')
@@ -54,3 +93,15 @@ assert is_match(r'c*a*t', 'chat')
 assert not is_match(r'ch*a*ts', 'chat')
 assert not is_match(r'.*at', 'chats')
 
+assert is_match_dp(r'ra.', 'ray')
+assert not is_match_dp(r'ra.', 'raymond')
+assert is_match_dp(r'*at', 'chat')
+assert is_match_dp(r'*chat', 'chat')
+assert is_match_dp(r'.*at', 'chat')
+assert is_match_dp(r'c*hat', 'chat')
+assert is_match_dp(r'.*hat', 'chat')
+assert is_match_dp(r'*', 'chat')
+assert is_match_dp(r'**', 'chat')
+assert is_match_dp(r'ch*a*t', 'chat')
+assert is_match_dp(r'c*a*t', 'chat')
+assert not is_match_dp(r'ch*a*ts', 'chat')
